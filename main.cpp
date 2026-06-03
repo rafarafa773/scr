7a51bc4b401d29b83166069d5e4a3f941861cc2f
rafarafa773 <rafael.fagundes@auxiliadora.net>
2026-06-03 10:19:56 -0300

    ok

--- main.cpp
+++ main.cpp
@@ -4,210 +4,194 @@
 #include <string>
 #include <filesystem>
 #include <iomanip>
-void print_with_highlight(const std::string& line, const std::string& target) {
-	size_t pos = line.find(target);
+
+void imprimir_con_resaltado(const std::string& linea, const std::string& objetivo) {
+	size_t pos = linea.find(objetivo);
 	if (pos == std::string::npos) {
-		std::cout << line << "\n";
+		std::cout << linea << "\n";
 		return;
     }
-	std::cout << line.substr(0, pos);
-	std::cout << "\033[31m" << target << "\033[0m";
-	std::cout << line.substr(pos + target.length()) << "\n";
+	std::cout << linea.substr(0, pos);
+	std::cout << "\033[31m" << objetivo << "\033[0m";
+	std::cout << linea.substr(pos + objetivo.length()) << "\n";
 }
 
-struct file_obj {
-	int lines = 0;
-	std::string line1;
-	std::size_t chars_of_file = 0;
+struct objeto_archivo {
+	int lineas = 0;
+	std::string linea1;
+	std::size_t caracteres_del_archivo = 0;
 
-	void reset_stream(std::ifstream& file1) {
-		file1.clear();
-		file1.seekg(0);
+	void reiniciar_flujo(std::ifstream& archivo1) {
+		archivo1.clear();
+		archivo1.seekg(0);
 	}
-	int help(char* prog_name) {
-		std::cout << "usage: " << prog_name << " <file> <option>\n\n"
-				  << "commands:\n"
-				  << "  list [dir]   : list files in directory (default: .)\n"
-				  << "  whihi        : print current working directory\n"
-				  << "  change <dir> : change working directory\n"
-				  << "  show         : display file content\n"
-				  << "  show <str>   : search for a string in file\n"
-				  << "  count        : count lines and characters\n"
-				  << "  hex          : display file in hexadecimal\n"
-				  << "  rm           : remove the file (dangerous!)\n"
-				  << "  tocar        : make a file\n"
-				  << "  tocar_legal  : make a directory\n"
-				  << "  modify       : write text\n";
+	int ayuda(char* nombre_prog) {
+		std::cout << "uso: " << nombre_prog << " <archivo> <opcion>\n\n"
+				  << "comandos:\n"
+				  << "  list [dir]     : listar archivos en directorio (por defecto: .)\n"
+				  << "  whihi          : imprimir directorio de trabajo actual\n"
+				  << "  change <dir>   : cambiar directorio de trabajo\n"
+				  << "  show           : mostrar contenido del archivo\n"
+				  << "  show <str>     : buscar un string en el archivo\n"
+				  << "  count          : contar lineas y caracteres\n"
+				  << "  hex            : mostrar archivo en hexadecimal\n"
+				  << "  rm             : eliminar el archivo (¡peligroso!)\n"
+				  << "  tocar          : crear un archivo\n"
+				  << "  tocar_legal    : crear un directorio\n"
+				  << "  modify         : escribir texto\n";
 		return 0;
 	}
 
-	int count_lines(std::ifstream& file1) {
-		reset_stream(file1);
-		lines = 0;
-		while (std::getline(file1, line1)) {
-			lines++;
+	int contar_lineas(std::ifstream& archivo1) {
+		reiniciar_flujo(archivo1);
+		lineas = 0;
+		while (std::getline(archivo1, linea1)) {
+			lineas++;
 		}
-		return lines;
+		return lineas;
 	}
 
-	size_t count_chars(std::ifstream& file1) {
-		reset_stream(file1);
-		chars_of_file = 0;
-		while (std::getline(file1, line1)) {
-			chars_of_file += line1.size();
-			chars_of_file++;
+	size_t contar_caracteres(std::ifstream& archivo1) {
+		reiniciar_flujo(archivo1);
+		caracteres_del_archivo = 0;
+		while (std::getline(archivo1, linea1)) {
+			caracteres_del_archivo += linea1.size();
+			caracteres_del_archivo++;
 		}
-		return chars_of_file;
+		return caracteres_del_archivo;
 	}
-	void list_files(const std::string& directory_path = ".") {
-			for (const auto& entry : std::filesystem::directory_iterator(directory_path)) {
-				std::cout << entry.path().filename().string() << "\n";
+	void listar_archivos(const std::string& ruta_directorio = ".") {
+			for (const auto& entrada : std::filesystem::directory_iterator(ruta_directorio)) {
+				std::cout << entrada.path().filename().string() << "\n";
 			}
 	}
 
-	int remove_archive(std::string name_of_archive) {
-		std::filesystem::path path1 = name_of_archive;
-		if(std::filesystem::remove(path1)) {
-			std::cout << name_of_archive << " removed\n";
+	int eliminar_archivo(std::string nombre_del_archivo) {
+		std::filesystem::path ruta1 = nombre_del_archivo;
+		if(std::filesystem::remove(ruta1)) {
+			std::cout << nombre_del_archivo << " eliminado\n";
 			return 0;
 		} else {
 			std::cout << "error\n";
 			return 1;
 		}
 	}
-	void print_dir() {
+	void imprimir_dir() {
 		std::cout << std::filesystem::current_path().string() << "\n";
 	}
 
-	int change1(const std::string& new_path) {
-		std::filesystem::current_path(new_path);
-		std::cout << "changed to: " << std::filesystem::current_path().string() << "\n";
+	int cambiar1(const std::string& nueva_ruta) {
+		std::filesystem::current_path(nueva_ruta);
+		std::cout << "cambiado a: " << std::filesystem::current_path().string() << "\n";
 		return 0;
 	}
 
-
-
-	void show_all(std::ifstream& file1) {
-		reset_stream(file1);
-		while(std::getline(file1, line1)) {
-			std::cout << line1 << "\n";
+	void mostrar_todo(std::ifstream& archivo1) {
+		reiniciar_flujo(archivo1);
+		while(std::getline(archivo1, linea1)) {
+			std::cout << linea1 << "\n";
 		}
 	}
-	/*
-	compensating lines lalalala
-	compensating lines lalalala
-	compensating lines lalalala
-	compensating lines lalalala
-	compensating lines lalalala
-	compensating lines lalalala
-	compensating lines lalalala
-	compensating lines lalalala
-	compensating lines lalalala
-	compensating lines lalalala
-	compensating lines lalalala
-	compensating lines lalalala
-	compensating lines lalalala
-	compensating lines lalalala
-	compensating lines lalalala
-	compensating lines lalalala
+	/* compensando líneas boludeces 
+	... 
 	*/
 };
 
-int main(int argc, char *file_passed[]) {
+int main(int argc, char *archivo_pasado[]) {
 	double version = 0.23;
 
-	if (argc > 1 && (std::string)file_passed[1] == "--version") {
+	if (argc > 1 && (std::string)archivo_pasado[1] == "--version") {
 		std::cout << "version: " << version << "\n";
 		return 0;
 	}
 	
-	if (argc > 1 && (std::string)file_passed[1] == "--help") {
-		file_obj obj1;
-		obj1.help(file_passed[0]);
+	if (argc > 1 && (std::string)archivo_pasado[1] == "--help") {
+		objeto_archivo obj1;
+		obj1.ayuda(archivo_pasado[0]);
 		return 0;
     }
-	if (argc > 1 && (std::string)file_passed[1] == "whihi") {
-		file_obj test1;
-		test1.print_dir();
+	if (argc > 1 && (std::string)archivo_pasado[1] == "whihi") {
+		objeto_archivo prueba1;
+		prueba1.imprimir_dir();
 		return 0;
 	}
-	if (argc > 1 && (std::string)file_passed[1] == "change") {
+	if (argc > 1 && (std::string)archivo_pasado[1] == "change") {
 		if (argc < 3) {
-			std::cout << "your stupid\n";
+			std::cout << "sos un boludo\n";
 			return 1;
 		}
-		file_obj test1;
-		test1.change1(file_passed[2]);
+		objeto_archivo prueba1;
+		prueba1.cambiar1(archivo_pasado[2]);
 		return 0;
-	} /*this shit dont works*/
+	}
 
 	if (argc < 2) {
-		std::cout << "mate, provide a file.\n";
+		std::cout << "che, proveé un archivo.\n";
 		return 1;
 	}
-	if (argc > 1 && (std::string)file_passed[1] == "list") {
-    	std::string dir_path = (argc > 2) ? file_passed[2] : ".";
-    	file_obj test1;
-   		test1.list_files(dir_path);
+	if (argc > 1 && (std::string)archivo_pasado[1] == "list") {
+    	std::string ruta_dir = (argc > 2) ? archivo_pasado[2] : ".";
+    	objeto_archivo prueba1;
+   		prueba1.listar_archivos(ruta_dir);
 		return 0;
 	}
 	
 	if (argc == 2) {
-		std::cout << "are you dumb? use count or show.\n";
+		std::cout << "¿sos bobo? usá count o show.\n";
 	}
-	if (argc > 2 && (std::string)file_passed[2] == "tocar") {
-		std::ofstream arquivo(file_passed[1]);
-		if (arquivo.is_open()) {
+	if (argc > 2 && (std::string)archivo_pasado[2] == "tocar") {
+		std::ofstream archivo(archivo_pasado[1]);
+		if (archivo.is_open()) {
 			return 0;
 		} else {
 			std::cout << "error\n";
 			return 1;
 		}
 	}
-	if(argc > 2 and (std::string)file_passed[2]=="tocar_legal") {
-		std::filesystem::create_directory(file_passed[1]);
+	if(argc > 2 and (std::string)archivo_pasado[2]=="tocar_legal") {
+		std::filesystem::create_directory(archivo_pasado[1]);
 	}
-	std::ifstream file(file_passed[1]); 
-	if(!file.is_open()) {
-		std::cout << "mate, that file doesn't exist.\n";
+	std::ifstream archivo(archivo_pasado[1]); 
+	if(!archivo.is_open()) {
+		std::cout << "che, ese archivo no existe.\n";
 		return 1;
 	}
 	
 
-	file_obj test1;
+	objeto_archivo prueba1;
 
 	if (argc > 2) {
-		if ((std::string)file_passed[2] == "show" and argc == 3) {
-			test1.show_all(file);
-		} else if ((std::string)file_passed[2] == "count") {
-			std::cout << "lines: " << test1.count_lines(file) << "\n";
-			std::cout << "chars: " << test1.count_chars(file) << "\n";
-		} else if ((std::string)file_passed[2] == "show" and argc > 3) {
-			std::string busca = file_passed[3];
-			std::string linha_temp;
-			test1.reset_stream(file);
-			while (std::getline(file, linha_temp)) {
-				if (linha_temp.contains(busca)) {
-					print_with_highlight(linha_temp, busca);
+		if ((std::string)archivo_pasado[2] == "show" and argc == 3) {
+			prueba1.mostrar_todo(archivo);
+		} else if ((std::string)archivo_pasado[2] == "count") {
+			std::cout << "líneas: " << prueba1.contar_lineas(archivo) << "\n";
+			std::cout << "caracteres: " << prueba1.contar_caracteres(archivo) << "\n";
+		} else if ((std::string)archivo_pasado[2] == "show" and argc > 3) {
+			std::string busqueda = archivo_pasado[3];
+			std::string linea_temp;
+			prueba1.reiniciar_flujo(archivo);
+			while (std::getline(archivo, linea_temp)) {
+				if (linea_temp.contains(busqueda)) {
+					imprimir_con_resaltado(linea_temp, busqueda);
 				}
 			}
-		} else if ((std::string)file_passed[2] == "hex") {
-			std::string linha_temp;
-			test1.reset_stream(file);
-			while (std::getline(file, linha_temp)) {
-				for(unsigned char c : linha_temp) {
+		} else if ((std::string)archivo_pasado[2] == "hex") {
+			std::string linea_temp;
+			prueba1.reiniciar_flujo(archivo);
+			while (std::getline(archivo, linea_temp)) {
+				for(unsigned char c : linea_temp) {
 					std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(c) << " ";
 				}
 				std::cout << "\n";
 			}
-		} else if((std::string)file_passed[2] == "rm") {
-			test1.remove_archive(file_passed[1]);
-		} else if((std::string)file_passed[2]=="modify") {
-			std::string linhatem1;
-			file.close();
-			std::ofstream arqyuvis(file_passed[1], std::ios::app);
-			while (std::getline(std::cin, linhatem1)) {
-				arqyuvis << linhatem1 << "\n";
+		} else if((std::string)archivo_pasado[2] == "rm") {
+			prueba1.eliminar_archivo(archivo_pasado[1]);
+		} else if((std::string)archivo_pasado[2]=="modify") {
+			std::string linea_temp1;
+			archivo.close();
+			std::ofstream archivos_var(archivo_pasado[1], std::ios::app);
+			while (std::getline(std::cin, linea_temp1)) {
+				archivos_var << linea_temp1 << "\n";
 			}
 		}
 	}
